e493694d4640eddb12b01df6c90620646f11e527
suuhyeony <vv5835@naver.com>
2021-03-31 10:13:14 +0900

    [edit] hide api key

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_FIFA_API_KEY;
+
 export async function getUserId(nickname) {
     const response = await axios.get(
         `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${nickname}`
         , {headers:{
-            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTM0MjQ4Mzg3OCIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2MTY3NDM2MDcsImV4cCI6MTYzMjI5NTYwNywiaWF0IjoxNjE2NzQzNjA3fQ.sQrrM4P2bwphpndetNOm3wfQKSGj8HiSt3PEqMTk5Y0"
+            "Authorization": API_KEY
         }}
     );
     // console.log(response)
@@ -15,7 +17,7 @@ export async function getUserInfo(accessid) {
     const response = await axios.get(
         `https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessid}`
         , {headers: {
-            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTM0MjQ4Mzg3OCIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiI1MDA6MTAiLCJuYmYiOjE2MTY3NDM2MDcsImV4cCI6MTYzMjI5NTYwNywiaWF0IjoxNjE2NzQzNjA3fQ.sQrrM4P2bwphpndetNOm3wfQKSGj8HiSt3PEqMTk5Y0"
+            "Authorization": API_KEY
         }}
     );
     return response.data;
